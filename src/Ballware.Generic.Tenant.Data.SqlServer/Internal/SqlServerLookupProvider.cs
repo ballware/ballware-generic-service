eacb7878facecc796aae19e22f5ed9af320bf0ef
Frank Ballmeyer <frank@ballmeyer.com>
2025-06-07 14:09:10 +0200

    Provide empty result when lookup by id doesn't find data record #21

--- src/Ballware.Generic.Tenant.Data.SqlServer/Internal/SqlServerLookupProvider.cs
+++ src/Ballware.Generic.Tenant.Data.SqlServer/Internal/SqlServerLookupProvider.cs
@@ -30,14 +30,14 @@ class SqlServerLookupProvider : ITenantLookupProvider
         return await ProcessSelectListForLookupAsync<T>(db, null, tenant, lookup, rights);
     }
 
-    public async Task<T> SelectByIdForLookupAsync<T>(Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id) where T : class
+    public async Task<T?> SelectByIdForLookupAsync<T>(Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id) where T : class
     {
         using var db = await StorageProvider.OpenConnectionAsync(tenant.Id);
         
         return await ProcessSelectByIdForLookupAsync<T>(db, null, tenant, lookup, claims, id);
     }
 
-    public async Task<T> SelectByIdForLookupAsync<T>(Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id) where T : class
+    public async Task<T?> SelectByIdForLookupAsync<T>(Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id) where T : class
     {
         using var db = await StorageProvider.OpenConnectionAsync(tenant.Id);
         
@@ -58,14 +58,14 @@ class SqlServerLookupProvider : ITenantLookupProvider
         return await ProcessSelectListForLookupWithParamAsync<T>(db, null, tenant, lookup, rights, param);
     }
 
-    public async Task<T> SelectByIdForLookupWithParamAsync<T>(Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id, string param) where T : class
+    public async Task<T?> SelectByIdForLookupWithParamAsync<T>(Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id, string param) where T : class
     {
         using var db = await StorageProvider.OpenConnectionAsync(tenant.Id);
         
         return await ProcessSelectByIdForLookupWithParamAsync<T>(db, null, tenant, lookup, claims, id, param);
     }
 
-    public async Task<T> SelectByIdForLookupWithParamAsync<T>(Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id, string param) where T : class
+    public async Task<T?> SelectByIdForLookupWithParamAsync<T>(Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id, string param) where T : class
     {
         using var db = await StorageProvider.OpenConnectionAsync(tenant.Id);
         
@@ -119,7 +119,7 @@ class SqlServerLookupProvider : ITenantLookupProvider
                 TenantPlaceholderOptions.Create()), new { tenantId = tenant.Id, claims = string.Join(",", rights) }, transaction);
     }
 
-    public async Task<T> ProcessSelectByIdForLookupAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id) where T : class
+    public async Task<T?> ProcessSelectByIdForLookupAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id) where T : class
     {
         if (string.IsNullOrEmpty(lookup.ByIdQuery))
         {
@@ -132,18 +132,18 @@ class SqlServerLookupProvider : ITenantLookupProvider
         queryParams[TenantVariableIdentifier] = tenant.Id;
         queryParams["id"] = id;
         
-        return (await db.QuerySingleAsync<dynamic>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id, lookup.ByIdQuery,
+        return (await db.QuerySingleOrDefaultAsync<T>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id, lookup.ByIdQuery,
             TenantPlaceholderOptions.Create()), queryParams, transaction));
     }
 
-    public async Task<T> ProcessSelectByIdForLookupAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id) where T : class
+    public async Task<T?> ProcessSelectByIdForLookupAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id) where T : class
     {
         if (string.IsNullOrEmpty(lookup.ByIdQuery))
         {
             throw new ArgumentException(nameof(lookup.ByIdQuery));
         }
         
-        return (await db.QuerySingleAsync<T>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id, lookup.ByIdQuery,
+        return (await db.QuerySingleOrDefaultAsync<T>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id, lookup.ByIdQuery,
             TenantPlaceholderOptions.Create()), new { id, tenantId = tenant.Id, claims = string.Join(",", rights) }, transaction));
     }
     
@@ -165,7 +165,7 @@ class SqlServerLookupProvider : ITenantLookupProvider
             TenantPlaceholderOptions.Create()), new { tenantId = tenant.Id, claims = string.Join(",", rights), param }, transaction);  
     }
 
-    public async Task<T> ProcessSelectByIdForLookupWithParamAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id, string param) where T : class
+    public async Task<T?> ProcessSelectByIdForLookupWithParamAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IDictionary<string, object> claims, object id, string param) where T : class
     {
         if (string.IsNullOrEmpty(lookup.ByIdQuery))
         {
@@ -179,20 +179,20 @@ class SqlServerLookupProvider : ITenantLookupProvider
         queryParams["param"] = param;
         queryParams["id"] = id;
         
-        return (await db.QuerySingleAsync<dynamic>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id,
+        return (await db.QuerySingleOrDefaultAsync<T>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id,
                 lookup.ByIdQuery,
                 TenantPlaceholderOptions.Create()),
             queryParams, transaction));     
     }
     
-    public async Task<T> ProcessSelectByIdForLookupWithParamAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id, string param) where T : class
+    public async Task<T?> ProcessSelectByIdForLookupWithParamAsync<T>(IDbConnection db, IDbTransaction? transaction, Metadata.Tenant tenant, Lookup lookup, IEnumerable<string> rights, object id, string param) where T : class
     {
         if (string.IsNullOrEmpty(lookup.ByIdQuery))
         {
             throw new ArgumentException(nameof(lookup.ByIdQuery));
         }
         
-        return (await db.QuerySingleAsync<dynamic>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id,
+        return (await db.QuerySingleOrDefaultAsync<T>(await StorageProvider.ApplyTenantPlaceholderAsync(tenant.Id,
                 lookup.ByIdQuery,
                 TenantPlaceholderOptions.Create()),
             new { id, tenantId = tenant.Id, claims = string.Join(",", rights), param }, transaction));
